61516c91bce11702bec67cf97b69f58246c1dc81
johnschatner <leontin2001@gmail.com>
2023-05-02 11:44:41 +0200

    Add HeroBanner

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 
 // Components
 import Header from "./components/Header/Header";
+import HeroBanner from "./components/HeroBanner/HeroBanner";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <HeroBanner></HeroBanner>
     </>
   );
 }
---
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,9 @@
+export default function HeroBanner() {
+  return (
+    <section className="hero-banner">
+      <div className="hero-banner-container">
+        <img src="https://placehold.co/600x400" alt="HERO-BANNER" />
+      </div>
+    </section>
+  );
+}

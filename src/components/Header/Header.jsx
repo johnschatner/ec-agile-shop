f440ffbc21c559e3c69c7224a88c2bfe76baf56e
johnschatner <leontin2001@gmail.com>
2023-05-02 10:06:59 +0200

    Update Header structure, added inside App.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,10 @@
 import "./Header.css";
 
-export default function Header() {}
+export default function Header() {
+  return (
+    <header>
+      <div className="header-left">left</div>
+      <div className="header-right">right</div>
+    </header>
+  );
+}

efdf16bc7e0dcfad6e254c01ad0c7eaac14624a6
johnschatner <leontin2001@gmail.com>
2023-05-02 11:15:06 +0200

    Add code comments

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,16 @@
+// Import necessary modules and styles
 import "./Menu.css";
 import { useState } from "react";
-
 import SubMenu from "./SubMenu";
 
+// Define a new React component named Menu
 export default function Menu() {
+  // Set up state for the current menu state (open or closed)
   const [menu, setMenu] = useState("closed");
 
+  // Define a click handler for the menu button
   const clickHandler = () => {
+    // Toggle the menu state between open and closed
     if (menu === "open") {
       setMenu("closed");
     } else {
@@ -14,11 +18,15 @@ export default function Menu() {
     }
   };
 
+  // Render the component's UI
   return (
     <>
+      {/* Button that toggles the menu when clicked */}
       <button className="btn" onClick={clickHandler}>
         Menu
       </button>
+
+      {/* SubMenu component that displays the menu options */}
       <SubMenu class={menu === "open" ? "open" : "closed"} />
     </>
   );
--- src/components/Menu/SubMenu.jsx
+++ src/components/Menu/SubMenu.jsx
@@ -1,24 +1,36 @@
+// Import necessary modules
 import { useEffect, useState } from "react";
 import "./SubMenu.css";
 
+// Define a new React component named SubMenu
 export default function SubMenu(props) {
+  // Set up state for the current level
   const [level, setLevel] = useState("");
 
+  // Define a click handler for the menu buttons
   const clickHandler = (props) => {
+    // Update the level state based on which button was clicked
     setLevel(props);
   };
 
+  // Set up an effect that logs the current level whenever it changes
   useEffect(() => {
     console.log(level);
   }, [level]);
 
+  // Render the component's UI
   return (
     <>
+      {/* Outer container for the submenu */}
       <section className={`submenu ${props.class}`}>
+        {/* Navigation container for the submenu */}
         <nav className="submenu-nav">
+          {/* If the level is not empty, show a "Back" button */}
           {level !== "" ? (
             <button onClick={() => clickHandler("")}>Back</button>
           ) : null}
+
+          {/* If the level is empty, show the top-level menu options */}
           {level === "" ? (
             <>
               <button onClick={() => clickHandler("cars")}>Bilar</button>
@@ -28,16 +40,22 @@ export default function SubMenu(props) {
               <button onClick={() => clickHandler("boats")}>Båtar</button>
             </>
           ) : null}
+
+          {/* If the level is "cars", show the detailed car menu */}
           {level === "cars" ? (
             <>
               <div>Bilar</div>
             </>
           ) : null}
+
+          {/* If the level is "airplanes", show the detailed airplane menu */}
           {level === "airplanes" ? (
             <>
               <div>Flygplan</div>
             </>
           ) : null}
+
+          {/* If the level is "boats", show the detailed boat menu */}
           {level === "boats" ? (
             <>
               <div>Båtar</div>

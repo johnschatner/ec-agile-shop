f3f32d51671482c2ae41a75fd901814811e2ae8a
johnschatner <leontin2001@gmail.com>
2023-05-04 13:36:34 +0200

    Add basic clickable categories component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./App.css";
 // Components
 import Header from "./components/Header/Header";
 import HeroBanner from "./components/HeroBanner/HeroBanner";
+import ClickableCategories from "./components/Categories/ClickableCategories/ClickableCategories";
 
 function App() {
   return (
     <>
       <Header></Header>
       <HeroBanner></HeroBanner>
+      <ClickableCategories></ClickableCategories>
     </>
   );
 }
---
+++ src/components/Categories/ClickableCategories/ClickableCategories.jsx
@@ -0,0 +1,12 @@
+import "./ClickableCategories.css";
+
+export default function ClickableCategories() {
+  return (
+    <section className="clickable-categories">
+      <a href="#">Kategori</a>
+      <a href="#">Kategori</a>
+      <a href="#">Kategori</a>
+      <a href="#">Kategori</a>
+    </section>
+  );
+}

918e3d0474c34183867d510cea21bc73e7ddd11a
johnschatner <leontin2001@gmail.com>
2023-05-05 12:53:20 +0200

    Add Firebase and relevant SDKs to allow for admin panel

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+// Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env
+    .VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase if process object is defined
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
 // Products
 import { products } from "../assets/products/products";
 
@@ -7,6 +29,32 @@ export const ShopContext = createContext(null);
 export const ShopContextProvider = (props) => {
   // Stuff
   const [PRODUCTS, setPRODUCTS] = useState(products);
+
+  // Fetch products from Firestore
+  useEffect(() => {
+    // addProductsToFirestore(); // You can call this function when needed
+    // fetchProducts();
+  }, []);
+
+  async function addProductsToFirestore() {
+    const productsCollection = collection(db, "products");
+
+    products.forEach(async (product) => {
+      try {
+        const docRef = await addDoc(productsCollection, product);
+        console.log(`Document written with ID: ${docRef.id}`);
+      } catch (e) {
+        console.error(`Error adding document: ${e}`);
+      }
+    });
+  }
+
+  async function fetchProducts() {
+    const productsCollection = collection(db, "products");
+    const productsSnapshot = await getDocs(productsCollection);
+    const productsList = productsSnapshot.docs.map((doc) => doc.data());
+    setPRODUCTS(productsList);
+  }
 
   console.log(PRODUCTS);
 

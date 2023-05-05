afdfad7f68d9c3d37aa2cab8954c94fcfdd49ab0
johnschatner <leontin2001@gmail.com>
2023-05-05 13:34:13 +0200

    Add Firestore integration with realtime updates and cached products

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -3,7 +3,15 @@ import { createContext, useState, useEffect } from "react";
 // Firebase
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  orderBy,
+  query,
+  getDocs,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
@@ -21,25 +29,44 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firestore
 const db = getFirestore(app);
 
-// Products
-import { products } from "../assets/products/products";
-
 export const ShopContext = createContext(null);
 
 export const ShopContextProvider = (props) => {
   // Stuff
-  const [PRODUCTS, setPRODUCTS] = useState(products);
+  const [PRODUCTS, setPRODUCTS] = useState([]);
 
-  // Fetch products from Firestore
+  function getCachedProducts() {
+    const cachedData = localStorage.getItem("products");
+    return cachedData ? JSON.parse(cachedData) : null;
+  }
+
+  function setCachedProducts(products) {
+    console.log("Caching products in local storage");
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  // Fetch products from Firestore if not cached
   useEffect(() => {
-    // addProductsToFirestore(); // You can call this function when needed
-    // fetchProducts();
+    // Try to load cached data from local storage
+    const cachedProducts = getCachedProducts();
+    if (cachedProducts) {
+      console.log("Fetching products from local storage");
+      setPRODUCTS(cachedProducts);
+    }
+
+    // Set up the real-time listener to update products when they change
+    const unsubscribe = fetchProducts();
+
+    // Clean up the real-time listener when the component is unmounted
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   async function addProductsToFirestore() {
     const productsCollection = collection(db, "products");
 
-    products.forEach(async (product) => {
+    PRODUCTS.forEach(async (product) => {
       try {
         const docRef = await addDoc(productsCollection, product);
         console.log(`Document written with ID: ${docRef.id}`);
@@ -49,14 +76,23 @@ export const ShopContextProvider = (props) => {
     });
   }
 
-  async function fetchProducts() {
+  function fetchProducts() {
     const productsCollection = collection(db, "products");
-    const productsSnapshot = await getDocs(productsCollection);
-    const productsList = productsSnapshot.docs.map((doc) => doc.data());
-    setPRODUCTS(productsList);
-  }
+    const q = query(productsCollection, orderBy("id"));
 
-  console.log(PRODUCTS);
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const productsList = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        data.id = parseInt(data.id, 10); // Convert the id back to a number
+        return data;
+      });
+      console.log("Fetching products from Firestore", productsList);
+      setPRODUCTS(productsList);
+      setCachedProducts(productsList); // Update the cached data in local storage
+    });
+
+    return unsubscribe;
+  }
 
   const getProduct = (id) => {
     const product = PRODUCTS.find((product) => product.id === id);
